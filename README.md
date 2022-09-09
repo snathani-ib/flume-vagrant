43e054b9bbcc3ae2ca7584be959ec2ac874cd292
snathani <snathani@infoblox.com>
2022-09-09 12:21:18 +0530

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # flume-vagrant
 flume-vagrant
+
+This repo brings up the VM, deploys flume on it and runs two agents  
+
+The two agents that run are:    
+flume-ng agent --conf /opt/apache-flume-1.9.0-bin/conf --conf-file /opt/apache-flume-1.9.0-bin/conf/flume.conf --name a1 Dflume.root.logger=INFO,console &> /opt/apache-flume-1.9.0-bin/conf/logs/agent.log &  
+    Telnet to logs  
+    Run "telnet localhost 44444" and send data here to see it in the logs  
+flume-ng agent --conf /opt/apache-flume-1.9.0-bin/conf --conf-file /opt/apache-flume-1.9.0-bin/conf/flume_custom.conf --name a1 Dflume.root.logger=INFO,console &> /opt/apache-flume-1.9.0-bin/logs/agent_custom.log &  
+    File to logs  
+    The file /vagrant/test_flume.txt is going to be processed. 
+    
+
+
+
+
+
--- Ubuntu-flume/install_flume.sh
+++ Ubuntu-flume/install_flume.sh
@@ -17,15 +17,20 @@ flume-ng version
 
 cp /vagrant/flume.conf /opt/apache-flume-1.9.0-bin/conf/
 #flume-ng agent –conf ./conf/ -f conf/flume.conf -n agent1 -Dflume.root.logger=DEBUG
-flume-ng agent --conf conf --conf-file conf/flume.conf --name a1 Dflume.root.logger=INFO,console &> /opt/apache-flume-1.9.0-bin/logs/agent.log &
+cd /opt/apache-flume-1.9.0-bin/
+flume-ng agent --conf /opt/apache-flume-1.9.0-bin/conf --conf-file /opt/apache-flume-1.9.0-bin/conf/flume.conf --name a1 Dflume.root.logger=INFO,console &> /opt/apache-flume-1.9.0-bin/conf/logs/agent.log &
 
 echo "Please run this command: telnet localhost 44444 and send any characters to the console. You will be able to see the same characters in /opt/apache-flume-1.9.0-bin/logs/flume.log"
 
 #Custom plugin
+mkdir -p /opt/apache-flume-1.9.0-bin/logs
 cd /opt
 apt-get install maven -y
 git clone https://github.com/jjmleiro/flume-custom-source.git
-cd flume-custom-source.git
+cd flume-custom-source
 mvn package
 cp /opt/flume-custom-source/target/flume-custom-source-0.0.1-SNAPSHOT.jar /opt/apache-flume-1.9.0-bin/lib
-flume-ng agent --conf conf --conf-file conf/flume_custom.conf --name a1 Dflume.root.logger=INFO,console &> /opt/apache-flume-1.9.0-bin/logs/agent_custom.log &
+
+cp /vagrant/flume_custom.conf /opt/apache-flume-1.9.0-bin/conf/
+cd /opt/apache-flume-1.9.0-bin/
+flume-ng agent --conf /opt/apache-flume-1.9.0-bin/conf --conf-file /opt/apache-flume-1.9.0-bin/conf/flume_custom.conf --name a1 Dflume.root.logger=INFO,console &> /opt/apache-flume-1.9.0-bin/logs/agent_custom.log &
